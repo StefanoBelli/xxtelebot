fb289f945c3f4faa31d9ca0b5e2e1e1cb295b98a
StefanoBelli <sa.belli@hotmail.it>
2017-10-20 14:55:48 +0200

    Some corrections

--- src/api.cpp
+++ src/api.cpp
@@ -236,7 +236,7 @@ api_types::Chat tgbot::methods::Api::getChat(const std::string &chatId) const {
     Json::Value value;
     Json::Reader reader;
 
-    reader.parse(http::get(inst,baseApi + "/getChat?chat_id=\"" + chatId + "\""), value);
+    reader.parse(http::get(inst,baseApi + "/getChat?chat_id=" + chatId), value);
     curl_easy_cleanup(inst);
 
     if(!value.get("ok","").asBool())
@@ -251,7 +251,7 @@ unsigned tgbot::methods::Api::getChatMembersCount(const std::string &chatId) con
     Json::Value value;
     Json::Reader reader;
 
-    reader.parse(http::get(inst,baseApi + "/getChatMembersCount?chat_id=\"" + chatId + "\""), value);
+    reader.parse(http::get(inst,baseApi + "/getChatMembersCount?chat_id=" + chatId), value);
     curl_easy_cleanup(inst);
 
     if(!value.get("ok","").asBool())
@@ -266,7 +266,7 @@ api_types::File tgbot::methods::Api::getFile(const std::string &fileId) const {
     Json::Value value;
     Json::Reader reader;
 
-    reader.parse(http::get(inst,baseApi + "/getFile?file_id=\"" + fileId + "\""), value);
+    reader.parse(http::get(inst,baseApi + "/getFile?file_id=" + fileId), value);
     curl_easy_cleanup(inst);
 
     if(!value.get("ok","").asBool())
@@ -282,7 +282,7 @@ api_types::ChatMember tgbot::methods::Api::getChatMember(const std::string &chat
     Json::Reader reader;
 
     std::stringstream url;
-    url << baseApi << "/getChatMember?chat_id=\"" << chatId << "\"&user_id=" << userId;
+    url << baseApi << "/getChatMember?chat_id=" << chatId << "&user_id=" << userId;
 
     reader.parse(http::get(inst,url.str()), value);
     curl_easy_cleanup(inst);
@@ -299,7 +299,7 @@ api_types::StickerSet tgbot::methods::Api::getStickerSet(const std::string &name
     Json::Value value;
     Json::Reader reader;
 
-    reader.parse(http::get(inst,baseApi + "/getStickerSet?name=\"" + name + "\""), value);
+    reader.parse(http::get(inst,baseApi + "/getStickerSet?name=" + name), value);
     curl_easy_cleanup(inst);
 
     if(!value.get("ok","").asBool())
@@ -334,7 +334,7 @@ std::vector<api_types::ChatMember> tgbot::methods::Api::getChatAdministrators(co
     Json::Value value;
     Json::Reader reader;
 
-    reader.parse(http::get(inst,baseApi + "/getChatAdministrators?chat_id=\"" + chatId + "\""), value);
+    reader.parse(http::get(inst,baseApi + "/getChatAdministrators?chat_id=" + chatId), value);
     curl_easy_cleanup(inst);
 
     if(!value.get("ok","").asBool())
@@ -378,8 +378,8 @@ std::vector<api_types::GameHighScore> tgbot::methods::Api::getGameHighScores(con
     Json::Reader reader;
 
     std::stringstream url;
-    url << baseApi << "/getGameHighScores?user_id=" << userId << "&inline_message_id=\""
-            << inlineMessageId << "\"";
+    url << baseApi << "/getGameHighScores?user_id=" << userId << "&inline_message_id="
+            << inlineMessageId;
 
     reader.parse(http::get(inst,url.str()), value);
     curl_easy_cleanup(inst);
