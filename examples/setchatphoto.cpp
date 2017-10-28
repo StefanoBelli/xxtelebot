45055a5f0350b3df696d42b2072083eddca481fc
deni2312 <denisbeqi2312@gmail.com>
2017-10-28 18:54:39 +0200

    Update setchatphoto.cpp

--- examples/setchatphoto.cpp
+++ examples/setchatphoto.cpp
@@ -5,13 +5,10 @@ using namespace tgbot;
 using namespace types;
 using namespace methods;
 
-void PinMsg(const Message message, const Api& api)
-{
-	try
-	{
-		api.setChatPhoto(std::to_string(message.chat.id),"example.jpg","jpg");
-	}catch(TelegramException& excp)
-		{
+void PinMsg(const Message message, const Api& api){
+	try{
+		api.setChatPhoto(std::to_string(message.chat.id),"example.jpg","image/jpeg");
+	}catch(TelegramException& excp){
 			std::cerr<<"Exception catched, it says:" << excp.what() << "\n";
 		}
 }
