c7fef0afc90642f85bdc774a4766e525aa07ac00
deni2312 <denisbeqi2312@gmail.com>
2017-10-28 17:38:44 +0200

    Add files via upload

---
+++ examples/pin.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <tgbot/bot.h>
+#include<iostream>
+using namespace tgbot;
+using namespace types;
+using namespace methods;
+
+void PinMsg(const Message message, const Api& api)
+{
+
+	try
+	{
+		api.pinChatMessage(std::to_string(message.chat.id), std::to_string(message.messageId),false);
+	}catch(TelegramException& excp)
+		{
+			std::cerr<<"Exception catched, it says:" << excp.what() << "\n";
+		}
+}
+
+int main() {
+	LongPollBot bot("token");
+	bot.callback(PinMsg);
+	bot.start();
+	//unreachable code
+}
