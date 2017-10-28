110be5fc1082055f54d0ad1cf5acf52a18907f78
deni2312 <denisbeqi2312@gmail.com>
2017-10-28 18:52:38 +0200

    Update kickuser.cpp

--- examples/kickuser.cpp
+++ examples/kickuser.cpp
@@ -5,13 +5,10 @@ using namespace tgbot;
 using namespace types;
 using namespace methods;
 
-void KickMem(const Message message, const Api& api)
-{
-	try
-	{
+void KickMem(const Message message, const Api& api){
+	try{
 		api.kickChatMember(std::to_string(message.chat.id), message.from->id,-1);
-	}catch(TelegramException& excp)
-		{
+	   }catch(TelegramException& excp){
 			std::cerr<<"Exception catched, it says:" << excp.what() << "\n";
 			api.sendMessage(std::to_string(message.chat.id),"Could not kick this user");
 		}
